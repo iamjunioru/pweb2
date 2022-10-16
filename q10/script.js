3f7b1ce308c5b52277214f696b03553230a86516
Junior <iamjunioru@gmail.com>
2022-10-16 03:05:21 -0300

    solved

---
+++ q10/script.js
@@ -0,0 +1,11 @@
+let x = parseInt(prompt("digite um número: "));
+
+if (x >= 0){
+    document.write(x);
+    document.write(" - esse número é positivo!");
+}
+
+else if (x < 0){
+    document.write(x);
+    document.write(" - esse número é negativo");
+}
